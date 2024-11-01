9628775ce51abb5d308164ab62aa46a76fa114ad
ducc <spielerc@gmail.com>
2024-11-01 16:35:41 -0600

    Don't support inheritance

--- Libraries/videopanel/Code/UI/VideoPanel.razor.cs
+++ Libraries/videopanel/Code/UI/VideoPanel.razor.cs
@@ -15,20 +15,20 @@ public partial class VideoPanel : Panel, IVideoPanel
 	/// <summary>
 	/// The path/url of a video relative to VideoRoot.
 	/// </summary>
-	public virtual string VideoPath { get; set; }
+	public string VideoPath { get; set; }
 	/// <summary>
 	/// Specifies where the video may be found, whether it comes from a BaseFileSystem or a website.
 	/// Set this to <see cref="VideoRoot.WebStream"/> if VideoPath is a URL.
 	/// </summary>
-	public virtual VideoRoot VideoRoot { get; set; } = VideoRoot.MountedFileSystem;
+	public VideoRoot VideoRoot { get; set; } = VideoRoot.MountedFileSystem;
 	/// <summary>
 	/// If true, the video will automatically loop whenever HasReachedEnd is true.
 	/// </summary>
-	public virtual bool ShouldLoop { get; set; } = true;
-	public virtual bool AutoPlay { get; set; } = true;
-	public virtual bool StartMuted { get; set; } = false;
+	public bool ShouldLoop { get; set; } = true;
+	public bool AutoPlay { get; set; } = true;
+	public bool StartMuted { get; set; } = false;
 	
-	public virtual float Width
+	public float Width
 	{
 		get
 		{
@@ -45,7 +45,7 @@ public partial class VideoPanel : Panel, IVideoPanel
 			Style.Width = Length.Pixels( value );
 		}
 	}
-	public virtual float Height
+	public float Height
 	{
 		get
 		{
@@ -66,48 +66,48 @@ public partial class VideoPanel : Panel, IVideoPanel
 	/// <summary>
 	/// If true, the video is in the process of being loaded (e.g. from a remote server).
 	/// </summary>
-	public virtual bool IsLoading => _videoLoader?.IsLoading != false || ( IsPlaying && _sinceLastTextureUpdate > 0.2f );
+	public bool IsLoading => _videoLoader?.IsLoading != false || ( IsPlaying && _sinceLastTextureUpdate > 0.2f );
 	/// <summary>
 	/// If true, the video is playing, is not paused, and has not yet finished playing.
 	/// </summary>a
-	public virtual bool IsPlaying => !HasReachedEnd && VideoPlayer is not null && !VideoPlayer.IsPaused;
+	public bool IsPlaying => !HasReachedEnd && VideoPlayer is not null && !VideoPlayer.IsPaused;
 	/// <summary>
 	/// If true, this video has reached the end of the file. If ShouldLoop is enabled, the video will
 	/// automatically loop.
 	/// </summary>
-	public virtual bool HasReachedEnd => VideoPlayer != null && VideoPlayer.PlaybackTime >= VideoPlayer.Duration;
+	public bool HasReachedEnd => VideoPlayer != null && VideoPlayer.PlaybackTime >= VideoPlayer.Duration;
 
 	#region Controls
 	/// <inheritdoc cref="VideoPlayer.Pause"/>
-	public virtual void Pause() => VideoPlayer?.Pause();
+	public void Pause() => VideoPlayer?.Pause();
 
 	/// <inheritdoc cref="VideoPlayer.IsPaused"/>
-	public virtual bool IsPaused => VideoPlayer?.IsPaused == true;
+	public bool IsPaused => VideoPlayer?.IsPaused == true;
 
 	/// <inheritdoc cref="VideoPlayer.Resume"/>
-	public virtual void Resume() => VideoPlayer?.Resume();
+	public void Resume() => VideoPlayer?.Resume();
 
 	/// <inheritdoc cref="VideoPlayer.TogglePause"/>
-	public virtual void TogglePause() => VideoPlayer?.TogglePause();
+	public void TogglePause() => VideoPlayer?.TogglePause();
 
 	/// <inheritdoc cref="VideoPlayer.Seek(float)"/>
-	public virtual void Seek( float time ) => VideoPlayer?.Seek( time );
+	public void Seek( float time ) => VideoPlayer?.Seek( time );
 
 	/// <inheritdoc cref="VideoPlayer.Duration"/>
-	public virtual float Duration => VideoPlayer?.Duration ?? 0f;
+	public float Duration => VideoPlayer?.Duration ?? 0f;
 
 	/// <summary>
 	/// Returns the current playback time in seconds of the video, or if setting,
 	/// will seek to the specified playback time in seconds.
 	/// </summary>
-	public virtual float PlaybackTime
+	public float PlaybackTime
 	{
 		get => VideoPlayer?.PlaybackTime ?? 0f;
 		set => Seek( value );
 	}
 
 	/// <inheritdoc cref="VideoPlayer.Stop"/>
-	public virtual void Stop() => VideoPlayer?.Stop();
+	public void Stop() => VideoPlayer?.Stop();
 
 	/// <summary>
 	/// Provides access to the various audio-related properties of VideoPlayer such
@@ -134,8 +134,8 @@ public partial class VideoPanel : Panel, IVideoPanel
 	#endregion
 
 	// Internal state
-	protected VideoPlayer VideoPlayer { get; set; }
-	protected Texture VideoTexture { get; set; }
+	private VideoPlayer VideoPlayer { get; set; }
+	private Texture VideoTexture { get; set; }
 	private AsyncVideoLoader _videoLoader;
 	private Task<VideoPlayer> _videoLoadTask;
 	private TrackingAudioAccessor _audioAccessor;
@@ -211,13 +211,14 @@ public partial class VideoPanel : Panel, IVideoPanel
 			await _videoLoadTask;
 		}
 
-		OnPreVideoLoad();
+		VideoPlayer ??= new VideoPlayer();
+		VideoPlayer.OnTextureData = OnTextureData;
 
 		_cancelSource = new CancellationTokenSource();
-
 		// Cache the CancellationToken because Task.IsCanceled isn't whitelisted,
 		// and subsequent calls to PlayVideo would create a new CancellationTokenSource.
 		var cancelToken = _cancelSource.Token;
+
 		_videoLoadTask = LoadVideo( cancelToken );
 		VideoPlayer = await _videoLoadTask;
 		_videoLoadTask = null;
@@ -234,19 +235,9 @@ public partial class VideoPanel : Panel, IVideoPanel
 		_shouldPauseNextFrame = !AutoPlay;
 		ConfigureAudio();
 		UpdateBackgroundImage();
-		OnPostVideoLoad();
 	}
 
-	/// <summary>
-	/// Called during PlayVideo before LoadVideo.
-	/// </summary>
-	protected virtual void OnPreVideoLoad() 
-	{
-		VideoPlayer ??= new VideoPlayer();
-		VideoPlayer.OnTextureData = OnTextureData;
-	}
-
-	protected virtual void OnTextureData( ReadOnlySpan<byte> span, Vector2 size )
+	private void OnTextureData( ReadOnlySpan<byte> span, Vector2 size )
 	{
 		_sinceLastTextureUpdate = 0f;
 		int width = (int)size.x;
@@ -273,13 +264,12 @@ public partial class VideoPanel : Panel, IVideoPanel
 	/// <br/><br/>
 	/// Called in PlayVideo after a video is loaded, but before OnPostVideoLoad.
 	/// </summary>
-	protected virtual void ConfigureAudio()
+	private void ConfigureAudio()
 	{
-		if ( _audioAccessor is null )
+		_audioAccessor ??= new TrackingAudioAccessor()
 		{
-			_audioAccessor = new TrackingAudioAccessor();
-			_audioAccessor.Muted = StartMuted;
-		}
+			Muted = StartMuted
+		};
 		_audioAccessor.VideoPlayer = VideoPlayer;
 		_audioAccessor.Target = AudioSource;
 	}
@@ -289,7 +279,7 @@ public partial class VideoPanel : Panel, IVideoPanel
 	/// <br/><br/>
 	/// Called in PlayVideo after a video is loaded, but before OnPostVideoLoad.
 	/// </summary>
-	protected virtual void UpdateBackgroundImage()
+	private void UpdateBackgroundImage()
 	{
 		if ( VideoPlayer is null )
 			return;
@@ -297,18 +287,13 @@ public partial class VideoPanel : Panel, IVideoPanel
 		Style.SetBackgroundImage( VideoTexture );
 		StateHasChanged();
 	}
-
-	/// <summary>
-	/// Called at the very end of PlayVideo, only if the video is loaded successfully.
-	/// </summary>
-	protected virtual void OnPostVideoLoad() { }
 	
 
 	/// <summary>
 	/// Load whatever video is specified by the current VideoPath and VideoRoot, returning
 	/// an instance of a VideoPlayer. Called during PlayVideo.
 	/// </summary>
-	protected virtual async Task<VideoPlayer> LoadVideo( CancellationToken cancelToken )
+	private async Task<VideoPlayer> LoadVideo( CancellationToken cancelToken )
 	{
 		_videoLoader ??= new AsyncVideoLoader( VideoPlayer );
 
@@ -359,7 +344,7 @@ public partial class VideoPanel : Panel, IVideoPanel
 	/// If so, and if ShouldLoop is true, the playback time will be 
 	/// set to the start of the video.
 	/// </summary>
-	protected virtual void DetectAndHandleLoop()
+	private void DetectAndHandleLoop()
 	{
 		// We use a custom looping mechanism because VideoPlayer.Repeat seems to mess up PlaybackTime.
 		if ( ShouldLoop && HasReachedEnd )
